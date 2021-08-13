9edbbe80b34cecef588c170a5b4d29c4e084d459
david connaughton <david.a.connaughton@gmail.com>
2021-08-13 15:28:59 +0100

    Updated Settings

--- dlr_folk/settings.py
+++ dlr_folk/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['dlr-folk.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['dlr-folk.herokuapp.com', 'localhost', '127.0.0.1',
+                 'www.dlrfolk.ie', 'dlrfolk.ie', ]
 
 
 # Application definition
